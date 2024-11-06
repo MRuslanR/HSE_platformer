36fd217bdc407e2593c62f5b299eb98f3c1d09da
Ruslan <141405635+MRuslanR@users.noreply.github.com>
2024-11-06 20:44:47 +0300

    added shooting enemy

---
+++ PlatformerGame/Assets/enemies/bullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    // Удаляем пулю при столкновении с любым объектом
+    //    Destroy(gameObject);
+    //}
+}
